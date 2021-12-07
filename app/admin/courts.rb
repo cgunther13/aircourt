1696d24e45db87bdc76a149eec48ce2cd62cac81
Chris Gunther <cgunther13@gmail.com>
2021-12-07 20:35:55 +0000

    Whitelist columns in Court for ActiveAdmin

--- app/admin/courts.rb
+++ app/admin/courts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Court do
 
+ permit_params :renter_id, :court_type, :directions, :court_rules, :location, :photo, :max_guests
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

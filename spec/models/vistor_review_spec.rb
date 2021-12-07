c512e13e36c9f2435ca062ed40208739a9e28a45
Chris Gunther <cgunther13@gmail.com>
2021-12-07 20:35:40 +0000

    Validate presence of vistor_reviews.body

--- spec/models/vistor_review_spec.rb
+++ spec/models/vistor_review_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe VistorReview, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:body) }
+
     it { should validate_numericality_of(:score).is_less_than(10).is_greater_than_or_equal_to(1) }
 
     end

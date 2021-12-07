e37ed0d1fcce7e0d8bf67d9d47efb9727a021213
Chris Gunther <cgunther13@gmail.com>
2021-12-07 20:36:58 +0000

    Generate graphiti resources for courts

---
+++ app/resources/court_resource.rb
@@ -0,0 +1,17 @@
+class CourtResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :renter_id, :integer
+  attribute :court_type, :string_enum, allow: Court.court_types.keys
+  attribute :directions, :string
+  attribute :court_rules, :string
+  attribute :location, :string
+  attribute :photo, :string
+  attribute :max_guests, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end

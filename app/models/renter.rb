8686e12d990aa05199a892e07735be389e9c879d
Chris Gunther <cgunther13@gmail.com>
2021-12-07 20:35:40 +0000

    Validate presence of renters.password

--- app/models/renter.rb
+++ app/models/renter.rb
@@ -5,6 +5,8 @@ class Renter < ApplicationRecord
 
   # Validations
 
+  validates :password, :presence => true
+
   validates :username, :uniqueness => true
 
   validates :username, :presence => true

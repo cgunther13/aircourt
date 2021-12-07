721a5b6eae0b31640f25cd9de5501b7b85e21417
Chris Gunther <cgunther13@gmail.com>
2021-12-07 20:35:40 +0000

    Validate numericality of vistor_reviews.score

--- spec/models/vistor_review_spec.rb
+++ spec/models/vistor_review_spec.rb
@@ -12,5 +12,7 @@ RSpec.describe VistorReview, type: :model do
 
     describe "Validations" do
 
+    it { should validate_numericality_of(:score).is_less_than(10).is_greater_than_or_equal_to(1) }
+
     end
 end
